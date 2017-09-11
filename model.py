20d64a65314eb6ff79452c5254f62361ae7adb3d
Francis Engelmann <francis.engelmann@gmail.com>
2017-09-11 09:13:37 +0200

    Add files

---
+++ model.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+
+def gcn_layer(H, adj_norm, output_dim, scope):
+
+    with tf.variable_scope(scope):
+        Dsize = H.get_shape()[1].value
+        W = tf.Variable(tf.random_uniform((Dsize, output_dim)))
+        # W = tf.Print(W, [W], "W")
+        Z = tf.matmul(tf.matmul(adj_norm, H), W)
+        # H_out = tf.nn.relu(Z)
+        H_out = tf.nn.sigmoid(Z)
+
+    return H_out
+
+def gcn(adj, deg):
+
+    # id = tf.eye(34)
+    # W = tf.Variable(tf.random_uniform((34, 2)), trainable=True)
+    # y = tf.matmul(id, W)
+    # return y
+
+    num_nodes = adj.get_shape()[0].value  # number of nodes in the graph, number of points
+    num_classes = 2  # number of ground truth classes
+
+    # inputs X , H(0)
+    id = tf.eye(num_nodes)
+    H_0 = id  # initial hidden state
+
+    # Normalize adjacency matrix
+    A = adj + id  # A' = A + I
+    deg = deg + id  # D = D + I = deg_matrix(A+I)
+    # D' = D^(-1/2)
+    D = tf.sqrt(deg)
+    D = tf.reciprocal(D)
+    D = tf.where(tf.is_inf(D), tf.zeros_like(D), D)
+    adj_norm = tf.matmul(D, tf.matmul(A, D))  # normalization of A
+
+    H_1 = gcn_layer(H_0, adj_norm, num_classes, "L1")
+    H_2 = gcn_layer(H_1, adj_norm, num_nodes//2, "L2")
+    H_3 = gcn_layer(H_2, adj_norm, num_classes, "L3")
+    y = H_1
+
+    return y
+
+def gcn_prev(adj, deg):
+    num_nodes = adj.get_shape()[0].value  # number of nodes in the graph, number of points
+    num_classes = 2  # number of ground truth classes
+    print(num_nodes)
+
+    # inputs X , H(0)
+    id = tf.eye(num_nodes)
+    H = id  # initial hidden state
+    Dsize = H.get_shape()[1].value
+    W = tf.Variable(tf.random_uniform((Dsize, num_classes)))
+
+    # A' = A + I
+    A1 = adj + id
+    # D = D + I = deg_matrix(A+I)
+    deg = deg + id
+    # D' = D^(-1/2)
+    D1 = tf.sqrt(deg)
+    D2 = tf.reciprocal(D1)
+    D2 = tf.where(tf.is_inf(D2), tf.zeros_like(D2), D2)
+
+    # f(H,A) = relu(D'^(-1/2)*A'*D'^(-1/2) * H * W)
+    A_norm = tf.matmul(D2, tf.matmul(A1, D2))  # normalization of A
+    Z = tf.matmul(tf.matmul(A_norm, H), W)
+    y = tf.nn.relu(Z)
+    return y
---
+++ utils.py
@@ -0,0 +1,58 @@
+import networkx as nx
+import numpy as np
+
+
+def load_data_karate():
+    G = nx.karate_club_graph()
+
+
+    adj = nx.adjacency_matrix(G)
+    adj = adj.todense()
+
+    feats = np.zeros(np.shape(adj))
+    print("Node Degree")
+    for v in G:
+        feats[v,v] =  G.degree(v)
+        #print('%s %s' % (v, G.degree(v)))
+
+
+    labels = [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        2,
+        1,
+        1,
+        1,
+        1,
+        2,
+        2,
+        1,
+        1,
+        2,
+        1,
+        2,
+        1,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+    ]
+    return adj, feats, labels
+
+def load_data_3DSMR():
+    pass
